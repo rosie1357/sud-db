b526daaa3e5d443a94b172839afb39e8061b76e5
Rosalie Malsberger <RMalsberger@mathematica-mpr.com>
2021-07-27 11:38:27 -0400

    feat: add ci  testing for G tables

---
+++ python_local/sud_databook_tables/common/utils/stats.py
@@ -0,0 +1,58 @@
+
+import numpy as np
+from scipy import stats
+
+
+def two_proportions_confint(success_a, size_a, success_b, size_b, significance = 0.05, default_exception = 'NA'):
+    """
+    Code taken from https://stackoverflow.com/questions/47570903/confidence-interval-for-the-difference-between-two-proportions-in-python
+    
+    A/B test for two proportions;
+    given a success a trial size of group A and B compute
+    its confidence interval;
+    resulting confidence interval matches R's prop.test function
+
+    Parameters
+    ----------
+    success_a, success_b : int
+        Number of successes in each group
+
+    size_a, size_b : int
+        Size, or number of observations in each group
+
+    significance : float, default 0.05
+        Often denoted as alpha. Governs the chance of a false positive.
+        A significance level of 0.05 means that there is a 5% chance of
+        a false positive. In other words, our confidence level is
+        1 - 0.05 = 0.95
+
+    default_exception str : default value to return if any input values are invalid and stat cannot be calculated
+        (eg if value is string), default is NA
+
+    Returns
+    -------
+
+    confint : list
+        Confidence interval of the two proportion test
+    """
+
+    try:
+
+        prop_a = success_a / size_a
+        prop_b = success_b / size_b
+        var = prop_a * (1 - prop_a) / size_a + prop_b * (1 - prop_b) / size_b
+        se = np.sqrt(var)
+
+        # z critical value
+        confidence = 1 - significance
+        z = stats.norm(loc = 0, scale = 1).ppf(confidence + significance / 2)
+
+        # standard formula for the confidence interval
+        # point-estimtate +- z * standard-error
+        prop_diff = prop_b - prop_a
+        confint = prop_diff + np.array([-1, 1]) * z * se
+        
+        return list(confint)
+
+    except:
+        return default_exception
--- python_local/sud_databook_tables/write_db/classes/TableClassG.py
+++ python_local/sud_databook_tables/write_db/classes/TableClassG.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from .TableClass import TableClass
 from common.utils.calc_comparisons import calc_comparisons
+from common.utils.stats import two_proportions_confint
 
 class TableClassG(TableClass):
     """
@@ -30,23 +31,38 @@ class TableClassG(TableClass):
         # assign scol, excel_cols to specify cols to write to sheet
 
         self.scol = 2
-        self.excel_cols = [f"{col}_{suffix}" for col in self.base_cols + self.comp_cols for suffix in ['py','cy']] + [f"{col}_diff" for col in self.comp_cols]
+        self.excel_cols = [f"{col}_{suffix}" for col in self.base_cols + self.comp_cols for suffix in ['py','cy']] + [f"{col}_diff" for col in self.comp_cols] + ['ci']
 
         # create prepped df to write to tables
 
         self.prep_for_tables()
 
+    def get_stats(self):
+        """
+        Method get_stats to run tests on proportions to get confidence interval and pvalue and add cols to prepped_df
+
+        """
+
+        # get CI (returned as series of lists) and reformat to rounded tuples
+
+        confint = self.prepped_df_pre[[self.excel_cols[2], self.excel_cols[0], self.excel_cols[3], self.excel_cols[1]]].apply(lambda x: two_proportions_confint(*x, default_exception='DS'), axis=1)
+
+        self.prepped_df_pre['ci'] = confint.apply(lambda x: f'({"{:0.2f}".format(x[0]*100)}, {"{:0.2f}".format(x[1]*100)})' if type(x) == list else x)
+
+        return self.prepped_df_pre
 
     def prep_for_tables(self):
         """
-        Method prep_for_tables to create prepped df (comparisons dfs), pull sheet name from Excel template, and
+        Method prep_for_tables to create prepped df (comparisons and stat tests), pull sheet name from Excel template, and
         assign to class attributes
 
         """
 
         keep_cols = ['state'] + self.base_cols + self.comp_cols
 
-        self.prepped_df = calc_comparisons(data1 = self.prepped_df[keep_cols], data2 = self.prepped_df_p[keep_cols], 
-                                           join_on = 'state', diff_types = 'raw', join_suffixes = ('_cy','_py'), fill_na='.')
+        self.prepped_df_pre = calc_comparisons(data1 = self.prepped_df[keep_cols], data2 = self.prepped_df_p[keep_cols], 
+                                               join_on = 'state', diff_types = 'raw', join_suffixes = ('_cy','_py'), fill_na='.')
+
+        self.prepped_df = self.get_stats()
 
         self.sheet_name = self.get_sheet_name()
