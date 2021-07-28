fc295d6ef110c23d36319ae28de11023380225f4
Rosalie Malsberger <RMalsberger@mathematica-mpr.com>
2021-07-28 10:23:35 -0400

    feat: add additional stats funcs

--- python_local/sud_databook_tables/common/utils/stats.py
+++ python_local/sud_databook_tables/common/utils/stats.py
@@ -2,10 +2,41 @@
 import numpy as np
 from scipy import stats
 
+def two_proportions_test(success_a, size_a, success_b, size_b):
+    """
+    Code taken from http://ethen8181.github.io/machine-learning/ab_tests/frequentist_ab_test.html#Comparing-Two-Proportions
+
+    A/B test for two proportions;
+    given a success a trial size of group A and B compute
+    its zscore and pvalue
+    
+    Parameters
+    ----------
+    success_a, success_b : int
+        Number of successes in each group
+        
+    size_a, size_b : int
+        Size, or number of observations in each group
+    
+    Returns
+    -------
+
+    pvalue : float
+        p-value for the two proportion z-test
+    """
+    prop_a = success_a / size_a
+    prop_b = success_b / size_b
+    prop_pooled = (success_a + success_b) / (size_a + size_b)
+    var = prop_pooled * (1 - prop_pooled) * (1 / size_a + 1 / size_b)
+    zscore = np.abs(prop_b - prop_a) / np.sqrt(var)
+    one_side = 1 - stats.norm(loc = 0, scale = 1).cdf(zscore)
+    pvalue = one_side * 2
+    return pvalue
+
 
 def two_proportions_confint(success_a, size_a, success_b, size_b, significance = 0.05, default_exception = 'NA'):
     """
-    Code taken from https://stackoverflow.com/questions/47570903/confidence-interval-for-the-difference-between-two-proportions-in-python
+    Code taken from http://ethen8181.github.io/machine-learning/ab_tests/frequentist_ab_test.html#Comparing-Two-Proportions
     
     A/B test for two proportions;
     given a success a trial size of group A and B compute
@@ -56,3 +87,17 @@ def two_proportions_confint(success_a, size_a, success_b, size_b, significance =
 
     except:
         return default_exception
+
+
+def p_adjust_bh(p):
+    
+    # taken from https://stackoverflow.com/questions/7450957/how-to-implement-rs-p-adjust-in-python/33532498#33532498
+    
+    """Benjamini-Hochberg p-value correction for multiple hypothesis testing."""
+    
+    p = np.asfarray(p)
+    by_descend = p.argsort()[::-1]
+    by_orig = by_descend.argsort()
+    steps = float(len(p)) / np.arange(len(p), 0, -1)
+    q = np.minimum(1, np.minimum.accumulate(steps * p[by_descend]))
+    return q[by_orig]
