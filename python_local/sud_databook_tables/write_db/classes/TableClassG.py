67b19358b5d4aae29b2860e182a34e5cca225c8b
Rosalie Malsberger <RMalsberger@mathematica-mpr.com>
2021-07-30 11:20:47 -0400

    feat: add stat testing

--- python_local/sud_databook_tables/write_db/classes/TableClassG.py
+++ python_local/sud_databook_tables/write_db/classes/TableClassG.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import numpy as np
 
 from .TableClass import TableClass
 from common.utils.calc_comparisons import calc_comparisons
-from common.utils.stats import two_proportions_confint
+from common.utils.stats import two_proportions_confint, two_proportions_test, p_adjust_bh, format_p
 
 class TableClassG(TableClass):
     """
@@ -31,7 +32,7 @@ class TableClassG(TableClass):
         # assign scol, excel_cols to specify cols to write to sheet
 
         self.scol = 2
-        self.excel_cols = [f"{col}_{suffix}" for col in self.base_cols + self.comp_cols for suffix in ['py','cy']] + [f"{col}_diff" for col in self.comp_cols] + ['ci']
+        self.excel_cols = [f"{col}_{suffix}" for col in self.base_cols + self.comp_cols for suffix in ['py','cy']] + [f"{col}_diff" for col in self.comp_cols] + ['ci'] + ['pval']
 
         # create prepped df to write to tables
 
@@ -43,9 +44,19 @@ class TableClassG(TableClass):
 
         """
 
-        # get CI (returned as series of lists) and reformat to rounded tuples
+        # create list of cols needed to pass to stats functions (success_a, size_a, success_b, size_b)
 
-        confint = self.prepped_df_pre[[self.excel_cols[2], self.excel_cols[0], self.excel_cols[3], self.excel_cols[1]]].apply(lambda x: two_proportions_confint(*x, default_exception='DS'), axis=1)
+        stats_cols = [self.excel_cols[2], self.excel_cols[0], self.excel_cols[3], self.excel_cols[1]]
+
+        # get pvalues (must get individual pvalues then apply correction for multiple testing) and reformat
+
+        pvalues = p_adjust_bh(self.prepped_df_pre[stats_cols].apply(lambda x: two_proportions_test(*x, default_exception='DS'), axis=1))
+
+        self.prepped_df_pre['pval'] = pd.Series(pvalues).apply(lambda x: format_p(x))
+
+        # get CI (returned as series of lists) and reformat to be in format of (0.00, 0.00)
+
+        confint = self.prepped_df_pre[stats_cols].apply(lambda x: two_proportions_confint(*x, default_exception='DS'), axis=1)
 
         self.prepped_df_pre['ci'] = confint.apply(lambda x: f'({"{:0.2f}".format(x[0]*100)}, {"{:0.2f}".format(x[1]*100)})' if type(x) == list else x)
 
@@ -61,7 +72,7 @@ class TableClassG(TableClass):
         keep_cols = ['state'] + self.base_cols + self.comp_cols
 
         self.prepped_df_pre = calc_comparisons(data1 = self.prepped_df[keep_cols], data2 = self.prepped_df_p[keep_cols], 
-                                               join_on = 'state', diff_types = 'raw', join_suffixes = ('_cy','_py'), fill_na='.')
+                                               join_on = 'state', diff_types = 'raw', join_suffixes = ('_cy','_py'), fill_na='DS')
 
         self.prepped_df = self.get_stats()
 
